a06b5b3f4aba8f8a50bb25a6f61565bde96edb3f
Kevin agudelo <kevinagude25@gmail.com>
2023-11-18 15:46:39 -0500

    Cliente Commit

---
+++ src/views/Clientes.vue
@@ -0,0 +1,28 @@
+<template>
+    <div>
+      <h1>Consulta de Clientes</h1>
+      <ul>
+        <li v-for="cliente in clientes" :key="cliente.id">
+          {{ cliente.nombre }} - {{ cliente.email }}
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios'
+  
+  export default {
+    data() {
+      return {
+        clientes: []
+      };
+    },
+    mounted() {
+        axios
+        .get('http://127.0.0.1:8000/api/clientes')
+        .then(responde => (this.clientes = response.data.clientes))
+    },
+  };
+  </script>
+  
