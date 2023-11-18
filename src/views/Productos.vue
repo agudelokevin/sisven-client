9d7bc5eee4d4bf7f520294c54a40281ca034894c
Kevin agudelo <kevinagude25@gmail.com>
2023-11-18 15:56:45 -0500

    Productos Commit

---
+++ src/views/Productos.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <h1>Consulta de Productos</h1>
+      <ul>
+        <li v-for="producto in productos" :key="producto.id">
+          {{ producto.nombre }} - {{ producto.precio }}
+        </li>
+      </ul>
+    </div>
+  </template>
+
+<script>
+  import axios from 'axios'
+  
+  export default {
+    data() {
+      return {
+        productos: []
+      };
+    },
+    mounted() {
+        axios
+        .get('http://127.0.0.1:8000/api/clientes')
+        .then(responde => (this.productos = response.data.productos))
+    },
+  };
+  </script>
