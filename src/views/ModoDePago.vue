4ec0c32b449f0479a4ca152830016a259921d398
Kevin agudelo <kevinagude25@gmail.com>
2023-11-18 15:47:22 -0500

    Pagos Commit

---
+++ src/views/ModoDePago.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <h1>Consulta de Modos de Pago</h1>
+      <ul>
+        <li v-for="modoPago in modosPago" :key="modoPago.id">
+          {{ modoPago.nombre }}
+        </li>
+      </ul>
+    </div>
+  </template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  data() {
+    return {
+      metodopago: []
+    };
+  },
+  mounted() {
+      axios
+      .get('http://127.0.0.1:8000/metodopago')
+      .then(responde => (this.metodopago = response.data.metodopago))
+  },
+};
+</script>
