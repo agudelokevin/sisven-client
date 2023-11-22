3fb019fa35b19c7f88bb683cad0cb13455666455
Sebas y Juli <jgomez4967@campuscecep.edu.co>
2023-11-22 17:50:47 -0500

    index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,62 @@
-import Vue from 'vue';
-import VueRouter from 'vue-router';
-import Clientes from '../components/Clientes.vue';
-import Categorias from '../components/Categorias.vue';
-import ModoDePago from '../components/ModoDePago.vue';
-import Productos from '../components/Productos.vue';
+import { createRouter, createWebHashHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
 
-Vue.use(VueRouter);
+import EditarCategory from ' ../components/categories/EditarCategory.vue';
+import NewCategory from ' ../components/categories/NewCategory.vue';
+import Customers from '../views/Customers';
+import Categories from '../views/Categories';
+import Paymodes from '../views/Paymodes';
+import Products from '../views/Products';
 
 const routes = [
-  { path: '/clientes', component: Clientes },
-  { path: '/categorias', component: Categorias },
-  { path: '/modo-de-pago', component: ModoDePago },
-  { path: '/productos', component: Productos },
-];
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/customers',
+    name: 'Customers',
+    component: Customers
+  },
+  {
+    path: '/categories',
+    name: 'Categories',
+    component: Categories
+  },
+  {
+    panth: '/editar-category/:id',
+    name: 'EditarCategory',
+    component: EditarCategory
+  },
+  {
+    panth: '/add-category/:id',
+    name: 'NewCategory',
+    component: NewCategory
+  },
+  {
+    path: '/paymodes',
+    name: 'Paymodes',
+    component: Paymodes
+  },
+  {
+    path: '/products',
+    name: 'Products',
+    component: Products
+  },
+  {
+    path: '/about',
+    name: 'about',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  }
+]
 
-const router = new VueRouter({
+const router = createRouter({
+  history: createWebHashHistory(),
   routes
-});
+})
 
-export default router;
+export default router
