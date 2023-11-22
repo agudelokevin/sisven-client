2a11390a74bed9803c48a7d28446f87cc8a5483d
Sebas y Juli <jgomez4967@campuscecep.edu.co>
2023-11-22 17:48:47 -0500

    main.js actualización

--- src/main.js
+++ src/main.js
@@ -1,13 +1,24 @@
 import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+  import 'bootstrap/dist/css/bootstrap.min.css';
 
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
+  import { library } from '@fortawesome-svg-core';
+  import { faBank, faBuilding, faPencil, faPhone, faPlus, faTag, faTrash, faUser } from '@fortawesome/free-solid-svg-icons';
+  import { FontAwesomeIcon } from '@fortawesome/vue-fortawesome';
+  import { createApp } from 'vue';
 
-Vue.config.productionTip = false;
+  import App from './App.vue';
+  import router from '.router';
 
-new Vue({
-  router,
-  render: h => h(App),
-}).$mount('#app');
+  library.add(faPhone);
+  library.add(faUser);
+  library.add(faTrash);
+  library.add(faPencil);
+  library.add(faTag);
+  library.add(faBuilding);
+  library.add(faBank);
+  library.add(faPlus);
+
+  createApp(App)
+     .component("font-awesome-icon", FontAwesomeIcon)
+     .use(router)
+     .mount('#app')
